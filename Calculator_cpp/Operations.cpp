e0f153b1d2bdb0701e4e6fe5650e28a7c64ded45
Aleks_graf <vorontsov.a.v@yandex.ru>
2022-06-23 18:37:22 +0500

    init commit

---
+++ Calculator_cpp/Operations.cpp
@@ -0,0 +1,25 @@
+#include "Operations.h"
+
+template<typename T>
+inline T add(const T F, const T S)
+{
+	return F + S;
+}
+
+template<typename T>
+inline T subtract(const T F, const T S)
+{
+	return F - S;
+}
+
+template<typename T>
+inline T multiple(const T F, const T S)
+{
+	return F * S;
+}
+
+template<typename T>
+inline T divide(const T F, const T S)
+{
+	return F / S;
+}
---
+++ Calculator_cpp/Operations.h
@@ -0,0 +1,13 @@
+#pragma once
+
+template<typename T>
+T add(const T, const T);
+
+template<typename T>
+T subtract(const T, const T);
+
+template<typename T>
+T multiple(const T, const T);
+
+template<typename T>
+T divide(const T, const T);
